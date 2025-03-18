5d04e2fb497d9840b414a667f5a1a47b8266b48b
522-tech <76034758+522-tech@users.noreply.github.com>
2025-03-17 20:40:52 -0600

    Version final del juego

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos =[];
+
+function agregarAmigo(){
+const imputAmigo = document.getElementById("amigo");
+const nombreAmigo = imputAmigo.value.trim();
+
+if (nombreAmigo === ""){
+
+     alert("Debes ingresar un nombre valido");
+     return;
+}
+amigos.push(nombreAmigo);
+
+actualizarLista();
+
+imputAmigo.value = "";
+imputAmigo.focus();
+
+}
+
+
+function actualizarLista (){
+const listaAmigos = document.getElementById("listaAmigos");
+listaAmigos.innerHTML ="";
+
+amigos.forEach(amigo => {const li =document.createElement("li")
+
+    li.textContent = amigo;
+    listaAmigos.appendChild(li);
+});
+
+}
+
+function sortearAmigo(){
+
+    if ( amigos.length === 0){
+
+        alert("no se encuentran nombre registrados para el sorteo");
+        return
+    }
+  
+const indiceAleatorio = Math.floor(Math.random()*amigos.length)
+const amigosSorteados = amigos[indiceAleatorio];
+
+    const resultados = document.getElementById("resultado");
+    resultados.innerHTML = `<li>${amigosSorteados}</li>`;
+
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("btnAdicionar").addEventListener("click", agregarAmigo)
+    document.getElementById("btnSortear").addEventListener("click", sortearAmigo);
+});
+
